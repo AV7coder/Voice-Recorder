5d403f12f5f4a914c6526cad341761fca602f174
Aadiyta Vyas <74606669+AV7coder@users.noreply.github.com>
2020-11-18 09:51:50 +0530

    Add files via upload

---
+++ voice recorder.py
@@ -0,0 +1,22 @@
+from os import name
+from gtts import gTTS
+import os
+import speech_recognition as sr
+r = sr.Recognizer() # making a object named r which is the recognizer
+
+if __name__ == "__main__":
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in') #Using google for voice recognition.
+        title = input("Enter the file title: ")
+        file = gTTS(query) # makes the file
+        file.save(f'{title}.mp3')# saves the file
+        os.startfile(f'{title}.mp3') # opens the file
+    except Exception as w:
+        print(w)
+
+    
